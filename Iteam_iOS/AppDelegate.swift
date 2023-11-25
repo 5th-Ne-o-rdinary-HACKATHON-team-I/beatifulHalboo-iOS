9dbd29245109bb0799179b9ffba11f245b838d5a
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-11-25 17:17:17 +0900

    feat: FCM추가

--- Iteam_iOS/AppDelegate.swift
+++ Iteam_iOS/AppDelegate.swift
@@ -7,9 +7,35 @@
 
 import UIKit
 import IQKeyboardManagerSwift
+import Firebase
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+         print("Firebase registration token: \(String(describing: fcmToken))")
+
+         let dataDict: [String: String] = ["token": fcmToken ?? ""]
+         NotificationCenter.default.post(
+           name: Notification.Name("FCMToken"),
+           object: nil,
+           userInfo: dataDict
+         )
+         // TODO: If necessary send token to application server.
+         // Note: This callback is fired at each app startup and whenever a new token is generated.
+       }
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    
+    // Foreground(앱 켜진 상태)에서도 알림 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
 
     var window: UIWindow?
 
@@ -18,7 +44,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         IQKeyboardManager.shared.shouldResignOnTouchOutside = true // 화면 아무 곳이나 터치 시 키보드 dismiss
-        
+        FirebaseApp.configure()
+               
+               // 앱 실행 시 사용자에게 알림 허용 권한을 받음
+               UNUserNotificationCenter.current().delegate = self
+               
+               let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+               UNUserNotificationCenter.current().requestAuthorization(
+                   options: authOptions,
+                   completionHandler: { _, _ in }
+               )
+               
+               // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+               application.registerForRemoteNotifications()
+               
+               // 파이어베이스 Meesaging 설정
+               Messaging.messaging().delegate = self
         return true
     }
 
