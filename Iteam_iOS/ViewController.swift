9036ad60ff03a382e96321e71e82ad862abb9e9e
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-11-25 11:07:35 +0900

    Initial Commit

---
+++ Iteam_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Iteam_iOS
+//
+//  Created by 최지철 on 2023/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
