25034bde7d75782ea69f2ce97ba74039e978d30b
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-11-25 11:33:18 +0900

    feta: 폴더구조화

---
+++ Iteam_iOS/Model/tempModel.swift
@@ -0,0 +1,8 @@
+//
+//  tempModel.swift
+//  Iteam_iOS
+//
+//  Created by 최지철 on 2023/11/25.
+//
+
+import Foundation
---
+++ Iteam_iOS/Service/tempService.swift
@@ -0,0 +1,8 @@
+//
+//  tempService.swift
+//  Iteam_iOS
+//
+//  Created by 최지철 on 2023/11/25.
+//
+
+import Foundation
